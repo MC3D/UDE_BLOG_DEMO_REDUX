d9c54c30b0dd558d14d288814d191f9aaf84e539
MC3D <m.chapman.home@gmail.com>
2016-12-12 16:03:11 -0500

    passes control of PostsNew from to redux

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import { reduxForm} from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    // const handleSubmit = this.props.handleSubmit;
+    // const title = this.props.fields.title;
+    const { fields: { title, categories, content }, handleSubmit } =  this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
       <h3>Create A New Post</h3>
       <div className="form-group">
         <label htmlFor="title">Title</label>
-        <input type="text" className="form-control" id="title" />
-        <label htmlFor="categories">Categories</label>
-        <input type="text" className="form-control" id="categories">
-        <label htmlFor="content">Content</label>
-        <textarea type="text" className="form-control" id="content">
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <input type="text" className="form-control" id="title" {...title} />
       </div>
+
+      <div className="form-group">
+        <label htmlFor="categories">Categories</label>
+        <input type="text" className="form-control" id="categories"  {...categories}/>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="content">Content</label>
+        <textarea type="text" className="form-control" id="content" {...content}/>
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
@@ -22,5 +32,5 @@ class PostsNew extends Component {
 
 export default reduxForm({
   form: 'PostsNewForm',
-  field: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content']
 })(PostsNew);
