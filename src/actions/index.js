ab0119558ea865fae2b3df0f201818410551578e
MC3D <m.chapman.home@gmail.com>
2016-12-12 14:50:50 -0500

    refactors PostsIndex as a container

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ const API_KEY = '?key=thefoxhole';
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
-
+    type: FETCH_POSTS,
+    payload: request
   };
 }
